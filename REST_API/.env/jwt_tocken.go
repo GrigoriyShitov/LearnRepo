3f15b0f7ec793c153668a73cf7fb4140b9ae99ae
GrigoriyShitov <93396986+GrigoriyShitov@users.noreply.github.com>
2024-08-18 18:57:38 +0300

    fix

--- REST_API/.env/jwt_tocken.go
+++
@@ -1,7 +0,0 @@
-package secret
-
-const Jwt_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyIiwibmFtZSI6IkdyaXNoYSIsImlhdCI6MTUxNjIzOTAyMn0.cjbr5C2LRpiq_a6aEf6l78JFgwnMEmpH5ID72r9mi9A"
-
-var SecretKey = "SaNyA_VoR_and_Valera_Glist"
-
-var Dsn = "host=localhost user=grish password=1129 dbname=restapi port=5432 sslmode=disable"
