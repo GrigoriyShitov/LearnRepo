0711bf489abcf10e4221ef27020bfa7097a8608a
Grigoriy Shitov <412712@niuitmo.ru>
2024-08-28 23:31:16 +0300

    FINISH

--- REST_API/main.go
+++ REST_API/main.go
@@ -10,14 +10,14 @@ import (
 func main() {
 
 	h := handlers.HandlerInit()
-	h.Router.HandleFunc("/wallets/users", h.HomeHandler)                                                                                                   // home page
-	h.Router.Handle("/wallets/users/{idUser:[0-9]+}", controllers.JWTmiddleware(http.HandlerFunc(h.GetUserInfoHandler))).Methods(http.MethodGet)           // get user info
-	h.Router.Handle("/wallets/users/{idUser:[0-9]+}/{name}", controllers.JWTmiddleware(http.HandlerFunc(h.CreateNewUserHandler))).Methods(http.MethodPost) // create new user
-	h.Router.Handle("/wallets/users/{idUser:[0-9]+}", controllers.JWTmiddleware(http.HandlerFunc(h.CreateNewWalletHandler))).Methods(http.MethodPost)      // create new wallet
-	h.Router.HandleFunc("/wallets/users/{idUser:[0-9]+}/{idWallet:[0-9]+}", h.OperationListHandler).Methods(http.MethodGet)
-	h.Router.HandleFunc("/wallets/users/{idUser:[0-9]+}/{idWallet:[0-9]+}/{idOperation:[0-9]+}", h.OperationDeleteHandler).Methods(http.MethodDelete)
-	h.Router.HandleFunc("/wallets/users/{whoChange:[0-9]+}/{idUser:[0-9]+}/{role:admin|user}", h.ADMIN_EditRoleHandler).Methods(http.MethodPatch)
-	h.Router.HandleFunc("/wallets/users/{idUser:[0-9]+}/{idWallet:[0-9]+}/{type:deposit|withdraw}/{amount:[0-9]+}/{category}", h.MakeOperationHandler).Methods(http.MethodPost)
+	h.Router.HandleFunc("/wallets/users", h.HomeHandler)                                                                                                             // home page
+	h.Router.Handle("/wallets/users/{idUser:[0-9]+}", controllers.JWTmiddleware(http.HandlerFunc(h.GetUserInfoHandler))).Methods(http.MethodGet)                     // get user info
+	h.Router.Handle("/wallets/users/{idUser:[0-9]+}/{name}", controllers.JWTmiddleware(http.HandlerFunc(h.CreateNewUserHandler))).Methods(http.MethodPost)           // create new user
+	h.Router.Handle("/wallets/users/{idUser:[0-9]+}", controllers.JWTmiddleware(http.HandlerFunc(h.CreateNewWalletHandler))).Methods(http.MethodPost)                // create new wallet
+	h.Router.Handle("/wallets/users/{idUser:[0-9]+}/{idWallet:[0-9]+}", controllers.JWTmiddleware(http.HandlerFunc(h.OperationListHandler))).Methods(http.MethodGet) //get operations list
+	h.Router.Handle("/wallets/users/{idUser:[0-9]+}/{idWallet:[0-9]+}/{idOperation:[0-9]+}", controllers.JWTmiddleware(http.HandlerFunc(h.OperationDeleteHandler))).Methods(http.MethodDelete)
+	h.Router.Handle("/wallets/users/{whoChange:[0-9]+}/{idUser:[0-9]+}/{role:admin|user}", controllers.JWTmiddleware(http.HandlerFunc(h.ADMIN_EditRoleHandler))).Methods(http.MethodPatch)
+	h.Router.Handle("/wallets/users/{idUser:[0-9]+}/{idWallet:[0-9]+}/{type:deposit|withdraw}/{amount:[0-9]+}/{category}", controllers.JWTmiddleware(http.HandlerFunc(h.MakeOperationHandler))).Methods(http.MethodPost)
 	log.Println("API is running!")
 	http.ListenAndServe(":8080", h.Router)
 }
